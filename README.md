4c62c3564874a0c92d5c63b6998f3bc99e261d3f
Krishna Saraswathula <kc.saraswathula@gmail.com>
2022-08-14 11:38:19 +0530

    Added comments and modified README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Delmic
+
+Software-exercise-1:
+filename: sw-exercise-1
+Problem statement:
+Determing the distance of the bright spot from the centre of the image.
+
+Approach:
+1. Determining bright spot
+2. determining center of the image
+3. determining distance between center and bright spot.
+
+Usage: The User can load a file using the menu bar in GUI. Once the image is loaded, the calculated distance between the bright spot and center of the image is shown on the screen.
+
+Result : 
+After successful processing two windows are shown comparing the input and the resultant image highlighting the bright spot.
+The main application window shows the calculated distance.
+
+
+Software-exercise-2:
+filename: sw-exercise-2
+Problem statement:
+1. Develop a image viewer for loading and viewing an image file
+2. When the window is resized, the image is scaled to fit in the window.
+3. Create a menu entry to keep the aspect ratio of the image file.
+
+References:
+resizeEvent() reference from stackoverflow.
+
+Description:
+Step 1: A class which initializes and loads the GUI App. 
+There is a Menu bar which has two functionalities:
+	1.Loading a file and displaying it in the GUI window
+	2.Checkable option for maintaining aspect ratio
+
+Step 2: open a file using a QFileDialog widget. The user can select an image file from directories
+which gets displayed by a QLabel widget
+
+Step 3: Maintaining the aspect ratio of the image.
--- sw-exercise-1.py
+++ sw-exercise-1.py
@@ -1,115 +1,139 @@
 # -*- coding: utf-8 -*-
 """
-A GUI application implemented in PyQt for loading an image file typically consisting of 
-bright spots or blobs. The goal is to locate a spot in the image and calculate the distance between the bright spot
-and the centre of the image.
+A GUI application implemented in PyQt for viewing Images.
 
-I have completed this task with a simplistic approach using built in functionalities available in openCv library.
-This functionality can be enhanced further by using more modern functions.
-
-The User can load a file using the GUI. Once the image is loaded, the user is displayed the 
-calculated distance inside a label widget.
-
-Created on Sat Aug 13 20:03:38 2022
+Created on Sat Aug 13 13:51:51 2022
 
 @author: krishna
 """
-# importing OpenCv and PyQt
+
+# Importing PyQt and openCV.
 import cv2
-import numpy as np
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QDialog,QPushButton,QGraphicsView \
-     ,QLabel,QToolBar, QAction, QFileDialog,QMenuBar, QMenu, QMainWindow,QStatusBar,QHBoxLayout,QSizePolicy,QGridLayout
+from PyQt5.QtWidgets import QApplication,QWidget \
+     ,QLabel,QToolBar, QAction, QFileDialog,QMenuBar, QMenu, QMainWindow,QStatusBar,QHBoxLayout
 from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot, QFile, QIODevice,Qt,QEvent
 from PyQt5.QtGui import QImage, QPixmap,QFont
 
+import numpy as np
 
-class UI(QMainWindow): # loading the main window
+"""
+Step 1: A class which initializes and loads the GUI App. 
+There is a Menu bar which has two functionalities:
+1.Loading a file and displaying it in the GUI window
+2.Checkable option for maintaining aspect ratio
+"""
+
+
+class UI(QMainWindow):                               # load the Mainwindow of our GUI App
     def __init__(self):
         super().__init__()
-        self.setGeometry(100,100,500,500)
-        self.setWindowTitle("sw-exercise-2")
+        self.setGeometry(100,100,1200,1200)          
+        self.setWindowTitle("sw-exercise-1")                                
         #toolbar = QToolBar("Toolbar")
         #self.addToolBar(toolbar)
      
-        file_dialog=QAction("File/Open",self) 
+        file_dialog=QAction("File/Open",self)
         file_dialog.setStatusTip("Click to open a file")
-        file_dialog.triggered.connect(self.open_file) #Menu option which calls open_file()
-        menuBar= self.menuBar()
+        file_dialog.triggered.connect(self.open_file) # Menu option which calls open_file()
+        #toolbar.addSeparator()
+        aspect_ratio=QAction("View/Keep aspect ratio",self)
+        aspect_ratio.setCheckable(True)
+        aspect_ratio.setStatusTip("Check aspect ratio")
+        aspect_ratio.triggered.connect(self.maintain_aspectRatio)   # Menu option which calls maintain_aspectRatio()
+        #self.setStatusBar(QStatusBar(self))
+        menuBar= self.menuBar()                                 # create a menubar object
         file_menu = menuBar.addMenu("&Menu")
         file_menu.addAction(file_dialog)
-        self.label=QLabel("",self) # a label widget for displaying the calculated distance
+        file_menu.addSeparator()
+        file_menu.addAction(aspect_ratio)
+        
+        self.label=QLabel("",self)                         # a label widget for displaying the input image
         self.label.setAlignment(Qt.AlignCenter)
-        self.label.setFont(QFont("Sanserif", 8))
-        self.label.setGeometry(20, 20, 200,200)
+        self.label.setFont(QFont("Sanserif", 30))
+        self.label.setGeometry(0, 20, 1024, 1024)
+        #self.label.setMinimumSize(1, 1)
         self.label.setAlignment(Qt.AlignCenter)
+        self.label.setScaledContents(True)
+        hbox=QHBoxLayout()
+        hbox.addWidget(self.label)
+    
+        self.setLayout(hbox)
+        #self.installEventFilter(self)
+       
+    """
+    Step 2: open a file using a QFileDialog widget. The user can select an image file from directories
+    which gets displayed by a QLabel widget
+    """   
+        
     def open_file(self):
         self.file=QFileDialog.getOpenFileName(self,'Open file', 
           'C:/Users/krishna/Downloads/Delmic_Assignment',"Image files (*.jpg *.gif *.tif *.png)")
-        distance_from_center=self.brightspot_check()
-        print("the distance from centre:"+str(distance_from_center))
+        self.image=cv2.imread(self.file[0])
+        
+        self.imgheight,self.imgwidth,self.img_channels=self.image.shape
+        self.img_aspect_ratio=self.imgheight/self.imgwidth # calculating the original loaded image's aspect ratio
+        self.resize(1200, 1200)#setGeometry(100,100,1200,1200)
+        self.label.resize(self.imgwidth,self.imgheight)
+        self.pixmap=QPixmap(self.file[0])
+        # we use the label to display the image
+        self.label.setPixmap(self.pixmap)
+        
 
-    def brightspot_check(self):
-        image=cv2.imread(self.file[0])
-        #image=cv2.imread(image_filepath)
-        grayscale_img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('input_image',grayscale_img)
+    def resizeEvent(self, event):                   # This triggers a resize event
+        # pixmap1 = QPixmap()
+        # pixmap = pixmap1.scaled(self.width(), self.height(),Qt.KeepAspectRatio)
+        self.label.setPixmap(self.pixmap)
+        self.label.resize(self.width(), self.height())  
+        self.window_aspectratio=self.height()/self.width()
+        QMainWindow.resizeEvent(self, event)
         
-            
-        """ 
-        We can use cv2.SimpleBlobDetector() to detect blobs/bright spots present in our image. To use this function, one can pass parameters
-        to this function. These parameters are provided from the cv2.SimpleBlobDetector_Params() and can be set to different values.
-        After setting the parameters, we can pass the parameters to cv2.SimpleBlobDetector()
-        """
-        blob_parameters=cv2.SimpleBlobDetector_Params()
-        blob_parameters.filterByColor=True # A flag for filtering by color
-        blob_parameters.blobColor = 255
-        blob_parameters.filterByArea = True  # bool flag filtering by area/size of the blob
-        blob_parameters.minArea = 1       # Filter by area/size of the blob
-        blob_parameters.minThreshold=0       # minimum intensity threshold
-        #blob_parameters.maxThreshold=255     # maximum intensity threshold
-        blob_parameters.filterByCircularity = True
-        blob_parameters.filterByConvexity = True
-        blob_parameters.minConvexity = 0.01
-        blob_parameters.minCircularity = 0.001
-        blob_parameters.filterByInertia = True;
-        blob_parameters.minInertiaRatio = 0.01;
+    # def eventFilter(self,source, event):
+    #     if (source is self and event.type() == QEvent.Resize):
+    #        # self.pixmap_scaled= self.pixmap.scaled(self.size(),Qt.KeepAspectRatio)
+    #         self.label.setPixmap(self.pixmap_scaled)
+    #         #self.label.resize(self.width(), self.height())   
+    #     return super( UI, self).eventFilter(source, event)    
     
-        # we pass the parameters to a function which detects the blobs
-        blob_detectors=cv2.SimpleBlobDetector_create(blob_parameters)
-        detected_points=blob_detectors.detect(grayscale_img)
-        print("The number of blobs detected:"+str(len(detected_points)))
-        print(f"the location of the bright spot is x: {detected_points[0].pt[0]} and y: {detected_points[0].pt[1]}")
-        
-        """
-        Now that we have detected bright spots in our image, we can use drawKeypoints() method in opencv to identify
-        these spots on the input image
-        """
-        
-        finalimage_with_points = cv2.drawKeypoints(grayscale_img, detected_points,outImage=None, color=(0,0,255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        
-        image_centre_x=int(grayscale_img.shape[0]/2) # finding x coord of image centre
-        image_centre_y=int(grayscale_img.shape[1]/2) # finding y coord of image centre
-        distance=np.sqrt(np.square(detected_points[0].pt[0]-image_centre_x) + np.square(detected_points[0].pt[1]-image_centre_y))
+    """
+    Step 3: Maintaining the aspect ratio of the image.
+    """
+    def maintain_aspectRatio(self):
+        print(self.pixmap.size())
+        if self.img_aspect_ratio!=self.window_aspectratio:
+            if self.height()<self.width():
+                self.new_imgheight=self.height()
+                self.new_imgwidth=int(self.new_imgheight/self.img_aspect_ratio)
+                print(self.new_imgheight,self.new_imgwidth)
+                resized_image=cv2.resize(self.image,(self.new_imgwidth,self.new_imgheight))
+                
+                bytesPerLine=self.img_channels*self.new_imgwidth
+                
+                convertToQtFormat = QImage(resized_image, self.new_imgwidth,self.new_imgheight , bytesPerLine, QImage.Format_RGB888)
+            elif self.width()<self.height():
+                self.new_imgwidth=self.width()
+                self.new_imgheight=int(self.new_imgwidth*self.img_aspect_ratio)
+                print(self.new_imgheight,self.new_imgwidth)
+                resized_image=cv2.resize(self.image,(self.new_imgwidth,self.new_imgheight))
+                
+                bytesPerLine=self.img_channels*self.new_imgwidth
+                # Here we convert Image data into QImage object
+                convertToQtFormat = QImage(resized_image, self.new_imgwidth,self.new_imgheight , bytesPerLine, QImage.Format_RGB888)
 
-        # Show blobs
-        cv2.imshow("detected_points", finalimage_with_points)
-        
-        
-        self.label.setText('distance between image center and spot centre:'+str(np.round(distance)))
-        if cv2.waitKey(0) & 0xff == 27:
-            cv2.destroyAllWindows()
-        return np.round(distance)
-    def resizeEvent(self, event):
-        self.label.resize(self.width(), self.height()) 
-    # filepath='C:/Users/krishna/Downloads/sw-exercise-1/spot1.tif'
-    # result=brightspot_check(filepath)    
-    
-    # print(f"Ther distance between the image cooordinates and the centre of the detected bright spot/blob is {result}")
+
+            # Pass the QImage object to convert into Pixmap object
+            self.pixmap_new=QPixmap.fromImage(convertToQtFormat)    
+            self.label.resize(self.new_imgheight,self.new_imgwidth)
+            # scaling the pixmap object
+            self.pixmap_scaled=self.pixmap_new.scaled(self.new_imgheight,self.new_imgwidth,Qt.KeepAspectRatio)
+            # we use the label to display the image
+            self.label.setPixmap(self.pixmap_scaled)
+ 
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ui = UI()
     ui.show()
-    sys.exit(app.exec_())
-    
+    sys.exit(app.exec_())       
